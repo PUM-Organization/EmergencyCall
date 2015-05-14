d4d23e09c6e99aff5137b92904b15d4cb714024f
Daniel <karwowskid26@gmail.com>
2015-05-14 02:16:35 +0200

    Extend ArrayList to ShiftRegisterList with max size definied

---
+++ app/src/main/java/com/pomorganization/helpers/ShiftRegisterList.java
@@ -0,0 +1,30 @@
+package com.pomorganization.helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Daniel on 5/14/2015.
+ */
+public class ShiftRegisterList<T> extends ArrayList<T> implements List<T> {
+    private int maxSize;
+
+    public ShiftRegisterList(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public boolean add(T o)
+    {
+        if(super.size() < maxSize)
+        {
+            super.add(o);
+        }
+        else
+        {
+            super.remove(0);
+            super.add(o);
+        }
+        return true;
+    }
+}
